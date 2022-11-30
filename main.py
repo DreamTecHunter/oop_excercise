a20ffa9c20a71e457d2a370b9860f9eef535f42b
DreamTecHunter <Tobias.laser@ymail.com>
2022-11-30 13:06:31 +0100

    done

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Employee(Person):
         self.department = Department.UNDEFINED if department is None else department
 
 
-class HeadOfDepartment(Employee):
+class HeadOfGroup(Employee):
     def __init__(self, first_name: str, last_name: str, sex: Sex, department: Department = None):
         super().__init__(first_name, last_name, sex, department)
 
@@ -39,13 +39,39 @@ class Company:
         return len([e for e in self.employees if type(e) == employee_type])
 
     def count_departments(self):
-        return
+        return len({e.department for e in self.employees})
+
+    def count_employees_per_department(self):
+        departments = {e.department: 0 for e in self.employees}
+        for e in self.employees:
+            departments[e.department] += 1
+        return departments
+
+    def get_biggest_department(self):
+        biggest_department = list(self.count_employees_per_department().keys())[0]
+        for key in self.count_employees_per_department().keys():
+            biggest_department = key if self.count_employees_per_department()[key] > \
+                                        self.count_employees_per_department()[
+                                            biggest_department] else biggest_department
+        return biggest_department
+
+    def get_sex_ration(self):
+        counter = {
+            Sex.MALE: 0,
+            Sex.FEMALE: 0
+        }
+        for employee in self.employees:
+            counter[employee.sex] += 1 / len(self.employees)
+        return counter
 
 
 if __name__ == '__main__':
     company = Company([
-        Person("a", "a", Sex.MALE),
         Employee("a", "a", Sex.MALE, Department.UNDEFINED),
-        HeadOfDepartment("a", "a", Sex.MALE, Department.UNDEFINED)
+        HeadOfGroup("a", "a", Sex.MALE, Department.MARKETING),
+        HeadOfGroup("a", "b", Sex.MALE, Department.MARKETING)
     ])
     print(company.count_employees_via_type(Employee))
+    print(company.count_departments())
+    print(company.get_biggest_department())
+    print(company.get_sex_ration())
